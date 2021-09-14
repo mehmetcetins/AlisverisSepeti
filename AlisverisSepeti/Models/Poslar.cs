798793a6b7f4299132238e03ee6f194180058c9f
x1saint <mehmetcetins@hotmail.com>
2021-09-14 12:46:06 +0300

    poslar tablosu model olarak eklendi.

---
+++ AlisverisSepeti/Models/Poslar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AlisverisSepeti.Models
+{
+    public partial class Poslar
+    {
+        public int PosId { get; set; }
+        public string PosBankaAdi { get; set; }
+        public string GecerliKartlar { get; set; }
+        public int DizilisSira { get; set; }
+        public string ApiUser { get; set; }
+        public string ApiPassword { get; set; }
+        public bool SifreliAlisverisVarmi { get; set; }
+        public bool SifreliOlsunmu { get; set; }
+        public bool Aktifmi { get; set; }
+        public bool TaksitVarmi { get; set; }
+    }
+}
