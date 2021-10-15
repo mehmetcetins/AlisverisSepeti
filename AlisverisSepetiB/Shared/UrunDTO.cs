235fe0db16db6e34a7ddf6c0fe7f332cb9da5b2c
x1saint <mehmetcetins@hotmail.com>
2021-10-15 17:18:56 +0300

    urunleri listelemek için kullanılacak verileri transfer etmek için urundto oluşturuldu.

---
+++ AlisverisSepetiB/Shared/UrunDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlisverisSepetiB.Shared
+{
+    public class UrunDTO
+    {
+        public string UrunAdi { get; set; }
+        public string MarkaAdi { get; set; }
+        public string Fiyati { get; set; }
+    }
+}
