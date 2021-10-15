5f255967533371500e495e731edfca7cce022ad3
x1saint <mehmetcetins@hotmail.com>
2021-10-15 17:19:27 +0300

    veri artık urundto listesi olarak gönderiliyor.

--- AlisverisSepetiB/Server/Controllers/HomeController.cs
+++ AlisverisSepetiB/Server/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlisverisSepetiB.Shared;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,30 +12,34 @@ namespace AlisverisSepetiB.Server.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
-        public async Task<List<string>> Get()
+        public async Task<List<UrunDTO>> Get()
         {
-            List<string> names = new List<string>();
+            List<UrunDTO> uruns = new List<UrunDTO>();
             try
             {
-                
+                UrunDTO urunt;
                 using (var context = new AlisverisSepeti.Models.AlisverisSepetiContext())
                 {
                     int c = 0;
-                    foreach (var urun in await context.Urunlers.AsNoTracking().Include(urun => urun.UrunlerDils.Where(d => d.Dil.Varsayilanmi == true)).ToListAsync())
+                    foreach (var urun in await context.Urunlers.AsNoTracking().Include(urun=> urun.Urunozellikleris).Include(urun => urun.Marka).Include(urun => urun.UrunlerDils.Where(d => d.Dil.Varsayilanmi == true)).ToListAsync())
                     {
-                        names.Add(urun.UrunlerDils.FirstOrDefault()?.UrunAdi ?? "Ürün Adı Yok");
-
+                        urunt = new UrunDTO();
+                        urunt.UrunAdi = urun.UrunlerDils.FirstOrDefault()?.UrunAdi ?? "Ürün Adi Yok";
+                        urunt.MarkaAdi = urun.Marka.MarkaAdi;
+                        urunt.Fiyati = urun.Urunozellikleris.Where(ozellik => ozellik.OzellikAdi.Equals("Price")).FirstOrDefault()?.Deger ?? "0 TL";
+                        Console.Write(urunt);
+                        uruns.Add(urunt);
                     }
                 }
                 
             }
             catch(Exception e)
             {
-                
-                return names;
+                Console.WriteLine(e.InnerException.Message);
+                return uruns;
             }
             
-            return names;
+            return uruns;
             
         }
     }
