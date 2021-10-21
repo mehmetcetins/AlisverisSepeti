8dda07fa42f250577aa99d5eab07d18323004afb
x1saint <mehmetcetins@hotmail.com>
2021-10-21 16:56:56 +0300

    recursive tekrar denenecek.

--- AlisverisSepetiB/Server/Controllers/HomeController.cs
+++ AlisverisSepetiB/Server/Controllers/HomeController.cs
@@ -5,12 +5,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using AlisverisSepeti.Models;
 namespace AlisverisSepetiB.Server.Controllers
 {
     [Route("[controller]")]
     public class HomeController : Controller
     {
+
+
+        [HttpGet("Categories")]
+        public async Task<List<Urunkategoriler>> Categories()
+        {
+            int maxDepth;
+            List<Urunkategoriler> kategoriler;
+            using (var context = new AlisverisSepetiContext())
+            {
+                maxDepth = await context.Urunkategorilers.AsNoTracking().MaxAsync(kategori => kategori.Depth);
+                kategoriler = await context.Urunkategorilers.AsNoTracking().OrderBy(kategori => kategori.Depth).ToListAsync();
+                return kategoriler;
+            }
+            /*
+            int depth = 0;
+            List listem = new System.Collections.Generic.List();
+            foreach(var kategori in kategoriler)
+            {
+                if (kategori.Depth == depth + 1)
+                {
+                    ++depth;
+                }
+                if (kategori.Depth == depth)
+                {
+                    
+                }
+                
+            }*/
+        }
+
         [HttpGet]
         public async Task<List<UrunDTO>> Get()
         {
