93b1cc3b81200d4ab8e50c596e8efc97681c6f72
x1saint <mehmetcetins@hotmail.com>
2021-11-02 17:07:56 +0300

    sepet oluşturuldu ve containerdan veriyi okuyup listeliyor. Categories componentından alınacak ve ayrı bir component yapılacak

--- AlisverisSepetiB/Client/Program.cs
+++ AlisverisSepetiB/Client/Program.cs
@@ -1,3 +1,4 @@
+using AlisverisSepetiB.Client.StateContainers;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +18,7 @@ namespace AlisverisSepetiB.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Services.AddSingleton<BasketContainer>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMudServices();
             await builder.Build().RunAsync();
---
+++ AlisverisSepetiB/Client/StateContainers/BasketContainer.cs
@@ -0,0 +1,22 @@
+﻿using AlisverisSepetiB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlisverisSepetiB.Client.StateContainers
+{
+    public class BasketContainer
+    {
+        private List<UrunDTO> _basket = new List<UrunDTO>();
+        public List<UrunDTO> basket { get => _basket; }
+        public void AddNewItem(UrunDTO item)
+        {
+            _basket.Add(item);
+            NotifyStateChanged();
+        }
+
+        public Action OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
